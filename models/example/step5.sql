aad67c218428b41ac66a624fec84fac589ffa9bf
kailinkoch <68975515+kailinkoch@users.noreply.github.com>
2021-05-13 08:16:54 -0700

    final edits

--- models/example/step5.sql
+++ models/example/step5.sql
@@ -13,11 +13,3 @@ group by departure_airport, arrival_airport, num_flights
 
 
 
--- SELECT 
--- departure_airport,
--- arrival_airport,
--- num_flights,
--- avg(num_flights) OVER (partition by departure_airport)
--- into step_five
--- FROM step_four
--- group by departure_airport, arrival_airport, num_flights
